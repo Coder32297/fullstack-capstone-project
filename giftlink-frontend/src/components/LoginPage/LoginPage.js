651fb1afdbe866231239e98e53195fce90a251ba
Emma Rose Flaum <emma.flaum@gmail.com>
2026-09-18 10:44:43 -0400

    Remove unused import from LoginPage.js
    Removed unused import of useAppContext from AuthContext.

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,5 +1,4 @@
 import {urlConfig} from '../../config';
-import { useAppContext } from '../../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import './LoginPage.css';
